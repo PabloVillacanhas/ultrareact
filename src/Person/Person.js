6523acc5603e3f65dec91d626499a8b2c49300a7
Pablo Dominguez <pablo.dominguez@situm.es>
2019-09-10 10:42:50 +0200

    Set Person module as a stateless Component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react'
 
-export default class Person extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: props.name,
-      age: props.age,
-      info: props.info
-    }
-  }
-
-  componentWillReceiveProps({name, age, info}) {
-    this.setState({name: name, age:age, info:info})
-  }
-
-  render(){
-    return (<div>
-      "I'm {this.state.name} and i'm {this.state.age}."
-    </div>)
-  }
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name} and im {props.age} years old}</p>
+      {props.children}
+    </div>
+  )
 }
+
+export default person;
